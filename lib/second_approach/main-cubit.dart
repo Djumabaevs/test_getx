f15f303457e2ff93bafc5d7a0f1f71d203595377
BakytDzhumabaev <djumabaevb@gmail.com>
2021-12-18 12:38:56 +0600

    bloc consumer

--- lib/second_approach/main-cubit.dart
+++ lib/second_approach/main-cubit.dart
@@ -1,14 +1,14 @@
 import 'package:bloc/bloc.dart';
 
-class CounterCubit extends Cubit<int> {
-  CounterCubit() : super(0);
+class CounterCubit2 extends Cubit<int> {
+  CounterCubit2() : super(0);
 
   void increment() => emit(state + 1);
   void decrement() => emit(state - 1);
 }
 
 Future<void> main(List<String> args) async {
-  final cubit = CounterCubit();
+  final cubit = CounterCubit2();
 
   final streamSubscription = cubit.listen(print);
 
