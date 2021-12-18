bd9e96eaacf76a37073c86adbe6d62bbd49b7d81
BakytDzhumabaev <djumabaevb@gmail.com>
2021-12-18 12:12:31 +0600

    cubit with states

---
+++ lib/third_approach/cubit/counter_state.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+part of 'counter_cubit.dart';
+
+class CounterState {
+  int counterValue;
+  bool wasIncremented;
+
+  CounterState({@required this.counterValue, this.wasIncremented});
+}
