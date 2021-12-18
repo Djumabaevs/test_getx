25df579ba4d49c43aa07f45dd6adc85c8eaed6ce
BakytDzhumabaev <djumabaevb@gmail.com>
2021-12-18 11:54:58 +0600

    stream subscription

--- lib/main-bloc.dart
+++ lib/main-bloc.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 
 enum CounterEvent { incremenet, decrement }
@@ -6,5 +8,34 @@ class CounterBloc extends Bloc<CounterEvent, int> {
   CounterBloc() : super(0);
 
   @override
-  Stream<int> mapEventToState(CounterEvent event) async* {}
+  Stream<int> mapEventToState(CounterEvent event) async* {
+    switch (event) {
+      case CounterEvent.incremenet:
+        yield state + 1;
+        break;
+      case CounterEvent.decrement:
+        yield state - 1;
+        break;
+    }
+    throw UnimplementedError();
+  }
+}
+
+Future<void> main(List<String> args) async {
+  final bloc = CounterBloc();
+
+  final streamSubscription = bloc.listen(print);
+
+  bloc.add(CounterEvent.incremenet);
+  bloc.add(CounterEvent.incremenet);
+  bloc.add(CounterEvent.incremenet);
+
+  bloc.add(CounterEvent.decrement);
+  bloc.add(CounterEvent.decrement);
+  bloc.add(CounterEvent.decrement);
+
+  await Future.delayed(Duration.zero);
+
+  await streamSubscription.cancel();
+  await bloc.close();
 }
